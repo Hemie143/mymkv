7b9f629d602f61e699425ebddb511bf74a9e4064
Laurent Hemeryck <hemie143@gmail.com>
2018-04-23 16:55:58 +0200

    Added sample MKV and first tests

--- mymkv.py
+++ mymkv.py
@@ -1,26 +1,33 @@
 import sys
 import os.path
-import knowit
+# import knowit
+import libexec.MediaInfoDLL3 as mediainfo
 
 class videoMKV:
 
     def __init__(self, mkvfilename):
         self.mkvfilename = mkvfilename
         self.mkvfolder = os.path.dirname(mkvfilename)
+        self.mediainfo = mediainfo.MediaInfo()
+        self.mediainfo.Open(mkvfilename)
+        self.mediainfo.Option_Static("Complete", "1")
+        self.info = self.mediainfo.Inform()
 
 
 def main(args=None):
     import argparse
     parser = argparse.ArgumentParser(description='Encode my MKV')
-    parser.add_argument('-m', dest='filename', action='store')
+    parser.add_argument('-f', dest='filename', action='store')
     options = parser.parse_args(args)
     print(options.filename)
     if not os.path.isfile(options.filename):
         print('File does not exist')
         exit(1)
     mkv_input = videoMKV(options.filename)
-    test = knowit.know(options.filename, {'provider': 'mediainfo'})
-    print(test)
+    print(mkv_input.info)
+
+    # mediainfo test = knowit.know(options.filename, {'provider': 'mediainfo'})
+    # print(test)
 
 
 
