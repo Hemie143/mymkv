99ce2fabd6331494d9b9e802311fe65382041553
hemie143@gmail.com <1tb8NQ6PvKBw>
2018-04-25 23:07:52 +0200

    Start own mediainfo interface

--- libexec/MediaInfo.py
+++ libexec/MediaInfo.py
@@ -2,25 +2,28 @@ import os
 import sys
 from ctypes import *
 
-if os.name == "nt" or os.name == "dos" or os.name == "os2" or os.name == "ce":
-    _file = 'MediaInfo.dll'
-    _path = os.path.join(*(os.path.split(__file__)[:-1] + (_file,)))
-    _mod = ctypes.cdll.LoadLibary(_path)
-    # MediaInfoDLL_Handler = CDLL(_path)
-    MustUseAnsi = 0
-elif sys.platform == "darwin":
-    _file = 'libmediainfo.0.dylib'
-    _path = os.path.join(*(os.path.split(__file__)[:-1] + (_file,)))
-    MediaInfoDLL_Handler = CDLL(_path)
-    MustUseAnsi = 1
-else:
-    _file = 'libmediainfo.so.0'
-    _path = os.path.join(*(os.path.split(__file__)[:-1] + (_file,)))
-    MediaInfoDLL_Handler = CDLL(_path)
-    MustUseAnsi = 1
-
-
 class MediaInfo:
 
-    open = _mod.Open
-    open.argtypes = ()
+    def __init__(self, filename):
+        self.filename = filename
+
+        if os.name == "nt" or os.name == "dos" or os.name == "os2" or os.name == "ce":
+            _file = 'MediaInfo.dll'
+            _path = os.path.join(*(os.path.split(__file__)[:-1] + (_file,)))
+            # TODO: get _mod out of if-else
+            self._mod = cdll.LoadLibary(_path)
+            # MediaInfoDLL_Handler = CDLL(_path)
+            MustUseAnsi = 0
+        elif sys.platform == "darwin":
+            _file = 'libmediainfo.0.dylib'
+            _path = os.path.join(*(os.path.split(__file__)[:-1] + (_file,)))
+            # TODO : use _mod ?
+            MediaInfoDLL_Handler = CDLL(_path)
+            MustUseAnsi = 1
+        else:
+            _file = 'libmediainfo.so.0'
+            _path = os.path.join(*(os.path.split(__file__)[:-1] + (_file,)))
+            # TODO : use _mod ?
+            MediaInfoDLL_Handler = CDLL(_path)
+            MustUseAnsi = 1
+
