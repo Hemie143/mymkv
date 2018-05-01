b24d6eaefc1c56eb5ef30a32a06bcdc63e75ae0a
hemie143@gmail.com <1tb8NQ6PvKBw>
2018-05-01 22:20:59 +0200

    Not much time to do something...

--- libexec/MediaInfo.py
+++ libexec/MediaInfo.py
@@ -10,7 +10,9 @@ class MediaInfo:
         if os.name == "nt" or os.name == "dos" or os.name == "os2" or os.name == "ce":
             _file = 'MediaInfo.dll'
             _path = os.path.join(*(os.path.split(__file__)[:-1] + (_file,)))
+            print(_path)
             # TODO: get _mod out of if-else
+            MediaInfoDLL_Handler = CDLL(_path)
             self._mod = cdll.LoadLibary(_path)
             # MediaInfoDLL_Handler = CDLL(_path)
             MustUseAnsi = 0
@@ -27,3 +29,7 @@ class MediaInfo:
             MediaInfoDLL_Handler = CDLL(_path)
             MustUseAnsi = 1
 
+
+
+# test = MediaInfo('../FPS_test_1080.mkv')
+# print(test._mod)
---
+++ test.py
@@ -0,0 +1,5 @@
+import libexec.MediaInfo as mediainfo
+
+
+test1 = mediainfo.MediaInfo('FPS_test_1080.mkv')
+print(test1)
