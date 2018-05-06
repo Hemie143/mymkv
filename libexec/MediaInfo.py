e50bab0d904b94088e5dbdf8584b7d90bb23d650
hemie143@gmail.com <1tb8NQ6PvKBw>
2018-05-06 14:23:56 +0200

    5 more minutes

--- libexec/MediaInfo.py
+++ libexec/MediaInfo.py
@@ -2,47 +2,77 @@ import os
 import sys
 from ctypes import *
 
+
+class Stream:
+    General, Video, Audio, Text, Other, Image, Menu, Max = list(range(8))
+
+
+class Info:
+    Name, Text, Measure, Options, Name_Text, Measure_Text, Info, HowTo, Max = list(range(9))
+
+
+class InfoOption:
+    ShowInInform, Reserved, ShowInSupported, TypeOfValue, Max = list(range(5))
+
+
+class FileOptions:
+    Nothing, Recursive, CloseAll, xxNonexx_3, Max = list(range(5))
+
+
 class MediaInfo:
 
-    def __init__(self, filename):
-        self.filename = filename
+    def __init__(self, mkvfilename):
+        self.mkvfilename = mkvfilename
 
         if os.name == "nt" or os.name == "dos" or os.name == "os2" or os.name == "ce":
             _file = 'MediaInfo.dll'
-            _path = os.path.abspath(os.path.join(os.path.dirname(__file__), _file))
-            print(_path)
-            print(libname)
+            self._path = os.path.abspath(os.path.join(os.path.dirname(__file__), _file))
+            print(self._path)
             # TODO: get _mod out of if-else
-            MediaInfoDLL_Handler = CDLL(_path)
+            MediaInfoDLL_Handler = CDLL(self._path)
             #self._mod = cdll.LoadLibary(_path)
-            self._mod = CDLL(_path)
+            self._mod = CDLL(self._path)
             # MediaInfoDLL_Handler = CDLL(_path)
             MustUseAnsi = 0
         elif sys.platform == "darwin":
             _file = 'libmediainfo.0.dylib'
-            _path = os.path.abspath(os.path.join(os.path.dirname(__file__), _file))
+            self._path = os.path.join(*(os.path.split(__file__)[:-1] + (_file,)))
             # TODO : use _mod ?
             MediaInfoDLL_Handler = CDLL(_path)
             MustUseAnsi = 1
         else:
             _file = 'libmediainfo.so.0'
-            _path = os.path.abspath(os.path.join(os.path.dirname(__file__), _file))
+            self._path = os.path.join(*(os.path.split(__file__)[:-1] + (_file,)))
             # TODO : use _mod ?
             MediaInfoDLL_Handler = CDLL(_path)
             MustUseAnsi = 1
-
-    def open(self):
 
         MediaInfo_New = self._mod.MediaInfo_New
         MediaInfo_New.argtypes = []
         MediaInfo_New.restype = c_void_p
 
-        self.Handle = MediaInfo_New
+        self._handle = MediaInfo_New()
+        print('handle: {}'.format(self._handle))
+
+
+
+    def Open(self, filename=None):
+        # self.Handle = MediaInfo_New
+
+        if filename:
+            print('Open has been provided a filename')
 
         MediaInfo_Open = self._mod.MediaInfo_Open
         MediaInfo_Open.argtypes = [c_void_p, c_wchar_p]
         MediaInfo_Open.restype = c_size_t
-        return self._mod.MediaInfo_Open(self.Handle, self._path)
+        return MediaInfo_Open(self._handle, self._path)
+
+    def Get(self, StreamKind, StreamNumber, Parameter, InfoKind=Info.Text, SearchKind=Info.Name):
+        MediaInfo_Get = self._mod.MediaInfo_Get
+        MediaInfo_Get.argtypes = [c_void_p, c_size_t, c_size_t, c_wchar_p, c_size_t, c_size_t]
+        MediaInfo_Get.restype = c_wchar_p
+
+        return MediaInfo_Get(self._handle, StreamKind, StreamNumber, Parameter, InfoKind, SearchKind)
 
 '''
 print("Get with Stream=General and Parameter='FileSize'")
--- test.py
+++ test.py
@@ -1,5 +1,42 @@
-import libexec.MediaInfo as mediainfo
+import libexec.MediaInfo as MediaInfo
 
 
-test1 = mediainfo.MediaInfo('FPS_test_1080.mkv')
+test1 = MediaInfo.MediaInfo('FPS_test_1080.mkv')
 print(test1._mod.__dict__)
+
+print(test1.Open())
+
+print(test1.Get(0, 0, "FileSize"))
+
+
+'''
+MI = MediaInfo()
+    def __init__(self):
+        self.Handle=self.MediaInfo_New()
+        self.MediaInfo_Option(self.Handle, "CharSet", "UTF-8")
+
+
+MI.Open("Example.ogg")
+    def Open(self, File):
+        if MustUseAnsi:
+            return self.MediaInfoA_Open (self.Handle, File.encode("utf-8"));
+        else:
+            return self.MediaInfo_Open (self.Handle, File);
+
+
+MI.Option_Static("Complete")
+print(MI.Inform())
+
+print(MI.Get(Stream.General, 0, "FileSize"))
+    def Get(self, StreamKind, StreamNumber, Parameter, InfoKind=Info.Text, SearchKind=Info.Name):
+        if MustUseAnsi:
+            return self.MediaInfoA_Get(self.Handle, StreamKind, StreamNumber, Parameter.encode("utf-8"), InfoKind, SearchKind).decode("utf_8", 'ignore')
+        else:
+            return self.MediaInfo_Get(self.Handle, StreamKind, StreamNumber, Parameter, InfoKind, SearchKind)
+
+MI.Close()
+    def Close(self):
+        return self.MediaInfo_Close(self.Handle)
+
+
+'''
